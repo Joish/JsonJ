9f5c9a2d166838ec90d305c9c688ccd08aa68236
unknown <joishbosco99@gmail.com>
2019-05-18 19:15:23 +0530

    Removed unwanted lines

--- JsonParser/JsonParser.py
+++ JsonParser/JsonParser.py
@@ -115,31 +115,3 @@ class JsonParser():
     def get_parsed_data(self):
         return self.data
         
-
-
-# sample = {
-#     "glossary": {
-#         "title": "example glossary",
-# 		"GlossDiv": {
-#             "title": "S",
-# 			"GlossList": {
-#                 "GlossEntry": {
-#                     "ID": "SGML",
-# 					"SortAs": "SGML",
-# 					"GlossTerm": "Standard Generalized Markup Language",
-# 					"Acronym": "SGML",
-# 					"Abbrev": "ISO 8879:1986",
-# 					"GlossDef": {
-#                         "para": "A meta-markup language, used to create markup languages such as DocBook.",
-# 						"GlossSeeAlso": [{'a':1234}, "XML"]
-#                     },
-# 					"GlossSee": "markup"
-#                 }
-#             }
-#         }
-#     }
-# }
-
-# js = JsonParser(sample)
-# js.get_parsed_data()
-# js.clear()
