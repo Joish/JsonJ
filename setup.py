d51437bcf410c9fb40823d157a45b89f8d38b392
unknown <joishbosco99@gmail.com>
2019-05-18 20:21:57 +0530

    minor upgrade

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'JsonJ',         # How you named your package folder (MyLib)
   packages = ['jsonj'],   # Chose the same as "name"
-  version = '0.1.3',      # Start with a small number and increase it with every change you make
+  version = '0.1.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A library to prase Json Tree data in-order to make things simpler',   # Give a short description about your library
   author = 'Joish J Bosco',                   # Type in your name
   author_email = 'joishbosco99@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Joish/JsonParser',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Joish/JsonParser/archive/v_0.1.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Joish/JsonParser/archive/v_0.1.3.tar.gz',    # I explain this later on
   keywords = ['Python 3', 'Json', 'Tree','Tree Traversal', 'Recursive'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           
