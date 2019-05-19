b1b9d8a8d5df791e204dd7db9c3bd50fe2e1f2fa
unknown <joishbosco99@gmail.com>
2019-05-19 14:51:05 +0530

    Fixed Readme and other issues

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setuptools.setup(
   name = 'JsonJ',         # How you named your package folder (MyLib)
   packages = ['jsonj'],   # Chose the same as "name"
-  version = '0.1.5',      # Start with a small number and increase it with every change you make
+  version = '0.1.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A library to prase Json Tree data in-order to make things simpler',   # Give a short description about your library
   long_description =long_description,
@@ -18,7 +18,7 @@ setuptools.setup(
   author = 'Joish J Bosco',                   # Type in your name
   author_email = 'joishbosco99@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Joish/JsonParser',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Joish/JsonParser/archive/v_0.1.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Joish/JsonJ/archive/v_0.1.5.tar.gz',    # I explain this later on
   keywords = ['Python 3', 'Json', 'Tree','Tree Traversal', 'Recursive'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           
